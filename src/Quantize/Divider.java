55a71502e000d4464c9297271962366709846779
Laurant Jo <shklaurant@gmail.com>
2019-02-27 20:15:11 +0900

    impl

--- src/Quantize/Divider.java
+++ src/Quantize/Divider.java
@@ -2,7 +2,6 @@ package quantize;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public final class Divider {
@@ -17,17 +16,37 @@ public final class Divider {
     private List<PossibleDividing> possibleDividings() {
         List<PossibleDividing> result = new ArrayList<>();
         List<Integer> possibility = possibility();
+        addPossibility(result, possibility);
         while(true) {
-            for(int index = 0; index < possibility.size(); index++) {
+            for(int index = 0; index < possibility.size() - 1; index++) {
                 if(index == 0) {
-                    if(possibility.get(index) > 1) {
-
+                    if(dividerCandidate(possibility, index) > 0) {
+                        possibility.set(index, dividerCandidate(possibility, index));
+                        break;
                     }
                 } else {
-
+                    if((dividerCandidate(possibility, index)) > previousDivider(possibility, index)) {
+                        possibility.set(index, dividerCandidate(possibility, index));
+                        for(int i = 0; i < index; i++) {
+                            possibility.set(i, possibility.get(index) - (index - i));
+                        }
+                        break;
+                    }
                 }
             }
         }
+    }
+
+    private void addPossibility(List<PossibleDividing> result, List<Integer> possibility) {
+        result.add(new PossibleDividing(new ArrayList<>(possibility)));
+    }
+
+    private Integer previousDivider(List<Integer> possibility, int index) {
+        return possibility.get(index-1);
+    }
+
+    private int dividerCandidate(List<Integer> possibility, int index) {
+        return possibility.get(index) - 1;
     }
 
     private List<Integer> possibility() {
---
+++ src/Quantize/dd.java
@@ -0,0 +1,4 @@
+package quantize;
+
+public class dd {
+}
